38aac70ca54f97413eaa8ba4bd5ee8d63d34620a
Roy <lxysac@foxmail.com>
2021-12-03 17:19:51 +0800

    添加项目文件。

---
+++ WebApi/WeatherForecast.cs
@@ -0,0 +1,16 @@
+using Humanizer;
+
+namespace WebApi
+{
+    public class WeatherForecast
+    {
+        public string DateFormat => Date.Humanize();
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
---
+++ xUnitTestProject/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace xUnitTestProject
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
